5099a012542c2ade97a59db186af098ef4bd8522
Bogdan <34404779+bogdan98@users.noreply.github.com>
2019-03-28 11:31:37 +0000

    Update Term.h

--- Term.h
+++ Term.h
@@ -2,14 +2,15 @@
 #include <iostream>
 #include <list>
 
+//class that defines a term, which can be any expression
 class Term
 {
 private:
-	double coeff;
-	double power;
-	std::string name;
-	std::string var;
-	std::string fullName;
+	double coeff; //coefficient
+	double power; //power
+	std::string name; //name of the function (sin, cos, etc. Used only with simple functions)
+	std::string var;  //expresion inside the function
+	std::string fullName; //full name of the function, which is displayed
 public:
 	Term();
 	Term(double c, double p, std::string nn, std::string vv);
@@ -20,10 +21,9 @@ public:
 	virtual void display();
 	virtual void setFullName();
 	void mult(double p);
-	virtual void red_coeff();
-	//bool isEqual(Term *t);
+	virtual void red_coeff(); //set coefficient equal to 1
 	virtual std::string getFullName();
-	virtual void setVar(std::string s1);
+	virtual void setVar(std::string s1); //set the expression inside the function to s1
 	virtual Term* deriv();
 };
 
